bca84080c894e309ee0174c2cdc9587bdd7637d1
datmt <thedat1705k2@gmail.com>
2023-06-26 09:03:26 +0700

    init commit

---
+++ java/src/main/java/datmt/learning/funtional_interface/Main.java
@@ -0,0 +1,10 @@
+package datmt.learning.funtional_interface;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ java/src/main/java/datmt/learning/funtional_interface/PlusFI.java
@@ -0,0 +1,7 @@
+package datmt.learning.funtional_interface;
+
+@FunctionalInterface
+public interface PlusFI<T> {
+    int plus(T num1, T num2);
+
+}
