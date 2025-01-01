7836ef521da516096b1d878a5225e0858ff6db35
datmt <thedat1705k2@gmal.com>
2025-01-01 23:48:05 +0700

    add liquibase

---
+++ liquibase-interaction/src/test/java/org/example/liquibaseinteraction/LiquibaseInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.liquibaseinteraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseInteractionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
